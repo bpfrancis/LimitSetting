619de6058c1d8f9db92e58cdb5491f9001c9c89d
bpf6qc <bpf6qc@virginia.edu>
2014-06-27 14:22:10 -0400

    stuff

--- plotMacro/drawContour_new.C
+++ plotMacro/drawContour_new.C
@@ -19,23 +19,12 @@
 
 void drawContour_new(TString scan="stop-bino", bool print=false) {
 
-  TString data_dir = "table";
-  TString output_dir = "hist";
-
-  gSystem->mkdir(output_dir,true);
-
-  bool useSmooth = false;
-  bool useCustomGetContour = false;
-
-  int diagonal = 1;
-  int legendFillColor = 16;
-
   gROOT->ForceStyle();
 
   gStyle->SetOptStat(0);
   gStyle->SetPalette(1);
 
-//For the temperature plots
+  //For the temperature plots
   gStyle->SetPadRightMargin(0.2);
   gStyle->SetPadLeftMargin(0.18);
   gStyle->SetPadTopMargin(0.1);
@@ -49,37 +38,27 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
   gStyle->SetLabelFont(42, "xyz");
   gStyle->SetLabelSize(0.04, "xyz");
 
+  TString output_dir = "hist";
 
-  //  TString option2D = "CONT4 Z";
+  TString datafile = "table/" + scan + ".table";
+
+  gSystem->mkdir(output_dir,true);
+
+  bool useSmooth = false;
+  bool useCustomGetContour = false;
+
+  int diagonal = 1;
+  int legendFillColor = 16;
+
   TString option2D = "COL Z";
 
   int xMin = 0;
-  int xMax = 5010;
+  int xMax = 960;
   int yMin = 0;
-  int yMax = 2025;
+  int yMax = 775;
 
   TString xLabel = "m_{Stop} [GeV]";
   TString yLabel = "m_{Bino} [GeV]";
-
-  /*
-  Double_t mst[29] = {110, 160, 185, 210, 235, 260, 285, 310, 335, 360, 385, 410, 460, 510, 560, 610, 660, 710, 810, 910, 1010, 1110, 1210, 1310, 1410, 1510, 1710, 2010, 5010};
-  Double_t mBino[31] = {25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300, 325, 375, 425, 475, 525, 575, 625, 675, 725, 825, 925, 1025, 1125, 1225, 1325, 1425, 1525, 1725, 2025};
-
-  const int nX = 30;
-  const int nY = 32;
-
-  Double_t xBins[nX+1];
-  xBins[0] = 0;
-  xBins[1] = 55;
-  for(int i = 1; i < 29; i++) xBins[i+1] = (mst[i] + mst[i-1])/2.;
-  xBins[30] = 6510;
-
-  Double_t yBins[nY+1];
-  yBins[0] = 0;
-  yBins[1] = 12.5;
-  for(int i = 1; i < 31; i++) yBins[i+1] = (mBino[i] + mBino[i-1])/2.;
-  yBins[32] = 2175;
-  */
 
   const int nX = 16;
   const int nY = 16;
@@ -97,7 +76,7 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
   for(int i = 1; i < nY; i++) yBins[i] = (mBino[i] + mBino[i-1])/2.;
   yBins[nY] = 775;
 
-  TFile* fout = new TFile(output_dir+ "/hist_exclusion_"+scan+".root","RECREATE");
+  TFile* fout = new TFile(output_dir+"/hist_exclusion_"+scan+".root","RECREATE");
 
   const int nxs = 2;
   TString xsname[nxs] = {"xsec_obs","xsec_exp"};
@@ -112,8 +91,6 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
   for(int i=0; i<nlimit; i++){
     h_limit[i] = new TH2D(limitname[i],limitname[i],nX,xBins,nY,yBins);
   }
-
-  TString datafile = data_dir + "/" + scan + ".table";
 
   std::ifstream fin;
   fin.open(datafile.Data());
@@ -136,19 +113,19 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
     h_xs[0]->Fill(xx,yy,obsLimit*xsec);
     h_xs[1]->Fill(xx,yy,expLimit*xsec);
 
-    h_limit[0]->Fill(xx,yy,obsLimit);
-    h_limit[1]->Fill(xx,yy,obsLimit*(1-oneSigma_L));
-    h_limit[2]->Fill(xx,yy,obsLimit*(1+oneSigma_H));
-    h_limit[3]->Fill(xx,yy,expLimit);
-    h_limit[4]->Fill(xx,yy,exp_m1s);
-    h_limit[5]->Fill(xx,yy,exp_p1s);
+    h_limit[0]->Fill(xx, yy, obsLimit);
+    h_limit[1]->Fill(xx, yy, obsLimit * (1 - oneSigma_L));
+    h_limit[2]->Fill(xx, yy, obsLimit * (1 + oneSigma_H));
+    h_limit[3]->Fill(xx, yy, expLimit);
+    h_limit[4]->Fill(xx, yy, exp_m1s);
+    h_limit[5]->Fill(xx, yy, exp_p1s);
 
   }// while
   fin.close();
 
   // fix pot holes for all TH2D's
-  for(int i=0; i<nxs; i++) fillPotHoles(h_xs[i],diagonal);
-  for(int i=0; i<nlimit; i++) fillPotHoles(h_limit[i],diagonal);
+  for(int i=0; i<nxs; i++) fillPotHoles(h_xs[i], diagonal);
+  for(int i=0; i<nlimit; i++) fillPotHoles(h_limit[i], diagonal);
 
   // common labels for every plot
   TLatex* lat = new TLatex(0.18,0.92,"#bf{CMS Preliminary}  #sqrt{s} = 8 TeV");
@@ -166,15 +143,14 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
   lat3->SetTextFont(43);
   lat3->SetTextSize(25);
 
-
   // for diagonal == 1
   TGraph* upperDiagonalRegion = new TGraph(3);
-  upperDiagonalRegion->SetPoint(0,TMath::Min(xMin,yMin),TMath::Min(xMin,yMin));
-  upperDiagonalRegion->SetPoint(1,TMath::Min(xMin,yMin),yBins[nY]);
-  upperDiagonalRegion->SetPoint(2,TMath::Max(xBins[nX],yBins[nY]),TMath::Max(xBins[nX],yBins[nY]));
+  upperDiagonalRegion->SetPoint(0, TMath::Min(xMin, yMin), TMath::Min(xMin, yMin));
+  upperDiagonalRegion->SetPoint(1, TMath::Min(xMin, yMin), yBins[nY]);
+  upperDiagonalRegion->SetPoint(2, TMath::Max(xBins[nX], yBins[nY]), TMath::Max(xBins[nX], yBins[nY]));
   upperDiagonalRegion->SetFillColor(16);
 
-  TLatex* lat_upperDiagonal = new TLatex(0.6,0.85,"#tilde{t} NLSP");
+  TLatex* lat_upperDiagonal = new TLatex(0.6, 0.85, "#tilde{t} NLSP");
   lat_upperDiagonal->SetNDC(true);
   lat_upperDiagonal->SetTextSize(0.04);
 
@@ -185,9 +161,7 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
   lowerDiagonalRegion->SetPoint(2,TMath::Max(xBins[nX],yBins[nY]),TMath::Max(xBins[nX],yBins[nY]));
   lowerDiagonalRegion->SetFillColor(16);
 
-  TLatex* lat_lowerDiagonal;
-  if(scan.Contains("WB")) lat_lowerDiagonal = new TLatex(0.6,0.25,"M_{bino} > M_{wino}");
-  else lat_lowerDiagonal = new TLatex(0.6,0.25,"#tilde{t} NLSP");
+  TLatex * lat_lowerDiagonal = new TLatex(0.6, 0.25, "#tilde{t} NLSP");
   lat_lowerDiagonal->SetNDC(true);
   lat_lowerDiagonal->SetTextSize(0.04);
 
@@ -219,9 +193,7 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
     can_limit->Print("",".pdf");
   }
 
-  TH2D* h_back;
-  if(scan.Contains("gB")) h_back = new TH2D("h_back_"+scan,";"+xLabel+";"+yLabel,100,xMin,xMax,100,yMin,yMax+500);
-  else h_back = new TH2D("h_back_"+scan,";"+xLabel+";"+yLabel,100,xMin,xMax,100,yMin,yMax);
+  TH2D * h_back = new TH2D("h_back_"+scan,";"+xLabel+";"+yLabel,100,xMin,xMax,100,yMin,yMax);
 
   std::cout << "Now get contours..." << std::endl;
 
@@ -348,11 +320,6 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
     leg_xmin -= 0.35;
     leg_xmax -= 0.35;
   }
-  if(scan.Contains("combined")){
-    leg_xmin = 0.2;
-    leg_xmax = 0.5;
-    legendFillColor = kBlue-10;
-  }
 
   TString legendTitle = "pp #rightarrow #tilde{t}#tilde{t}, #tilde{t} #rightarrow t + #tilde{#chi}^{0}_{2}, #tilde{#chi}^{0}_{2} #rightarrow #gamma + #tilde{#chi}^{0}_{1}";
 
@@ -374,13 +341,7 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
 
   double xv = 0.25;
   double yv = 0.25;
-  if(scan.Contains("gsq_W")){
-    yv = 0.18;
-  }
-  else if(scan.Contains("gB") || scan.Contains("gW") || scan.Contains("WB")) {
-    xv = 0.22;
-    yv = 0.3;
-  }
+  
   TLatex* lat4 = new TLatex(xv,yv,"Excluded");
   lat4->SetNDC(true);
   lat4->SetTextFont(43);
@@ -395,77 +356,74 @@ void drawContour_new(TString scan="stop-bino", bool print=false) {
   }
 
 
-  TCanvas* can_exclusionOnLimit;
-  if(scan.Contains("sms_gg") || scan.Contains("stop-bino")){
-    can_exclusionOnLimit = new TCanvas("can_exclusionOnLimit_"+scan,"can_exclusionOnLimit_"+scan,900,800);
-    can_exclusionOnLimit->SetLogz();
-    h_xs[0]->SetTitle(";M_{gluino} [GeV];M_{Neutralino} [GeV];95% CL cross section upper limit [pb]");
-    h_xs[0]->SetMinimum(0);
-    h_xs[0]->GetZaxis()->SetRangeUser(9.e-4, 2.1e-2); //durp
-    //h_xs[0]->GetZaxis()->SetNdivisions(210);
-    h_xs[0]->Draw(option2D);
+  TCanvas * can_exclusionOnLimit = new TCanvas("can_exclusionOnLimit_"+scan,"can_exclusionOnLimit_"+scan,900,800);
+  can_exclusionOnLimit->SetLogz();
+  h_xs[0]->SetTitle(";M_{gluino} [GeV];M_{Neutralino} [GeV];95% CL cross section upper limit [pb]");
+  h_xs[0]->SetMinimum(0);
+  //h_xs[0]->GetZaxis()->SetRangeUser(9.e-4, 2.1e-2); //durp
+  //h_xs[0]->GetZaxis()->SetNdivisions(210);
+  h_xs[0]->Draw(option2D);
 
-    curvS[0]->SetLineColor(kBlack);
-
-    curvS[0]->Draw("SAME L");
-
-    upperDiagonalRegion->SetFillColor(0);
-    upperDiagonalRegion->Draw("same f");
-
-    TLegend* leg2 = new TLegend(leg_xmin,leg_ymin,leg_xmax - 0.05,leg_ymax,legendTitle,"brNDC");
-    //    leg2->SetFillColor(legendFillColor);
-    leg2->SetFillColor(0);
-    leg2->SetLineColor(0);
-    leg2->SetBorderSize(0);
-    leg2->SetTextFont(62);
-    leg2->SetTextSize(0.03);
-    //    leg2->AddEntry("NULL","NLO+NLL Limits","h");
-    leg2->AddEntry("NULL", "m(#tilde{q}) >> m(#tilde{g})", "h");
-    leg2->AddEntry(curvS[0],"#sigma^{NLO-QCD}","L");
-    leg2->Draw("same");
-
-    lat->Draw("same");
-    lat2->Draw("same");
-
-    can_exclusionOnLimit->RedrawAxis();
-
-    if(print) {
-      can_exclusionOnLimit->Print("",".gif");
-      can_exclusionOnLimit->Print("",".pdf");
-    }
-
-    TFile* fsms = new TFile(output_dir+ "/hist_sms_gg_1jet_output.root","RECREATE");
-    fsms->cd();
-    h_xs[1]->Write("h2_95CL_limit");
-    exp1sigma_aroundExp->Write("contour_exp_1s_band");
-    curvS[3]->Write("contour_exp");
-    curvS[4]->Write("contour_exp_1s_down");
-    curvS[5]->Write("contour_exp_1s_up");
-    curvS[1]->Write("contour_theory_obs_1s_down");
-    curvS[2]->Write("contour_theory_obs_1s_up");
-    curvS[0]->Write("contour_obs");
-    h_xs[0]->Write("observed_limit_in_xsecUnit");
-    h_xs[1]->Write("expected_limit_in_xsecUnit");
-    h_limit[0]->Write("observed_limit_in_R");
-    h_limit[1]->Write("observed_limit_1s_down_in_R");
-    h_limit[2]->Write("observed_limit_1s_up_in_R");
-    h_limit[3]->Write("expected_limit_in_R");
-    h_limit[4]->Write("expected_limit_1s_down_in_R");
-    h_limit[5]->Write("expected_limit_1s_up_in_R");
-    can_exclusionOnLimit->Write();
-    fsms->Write();
-    fsms->Close();
+  curvS[0]->SetLineColor(kBlack);
+  
+  curvS[0]->Draw("SAME L");
+  
+  upperDiagonalRegion->SetFillColor(0);
+  upperDiagonalRegion->Draw("same f");
+  
+  TLegend* leg2 = new TLegend(leg_xmin,leg_ymin,leg_xmax - 0.05,leg_ymax,legendTitle,"brNDC");
+  //    leg2->SetFillColor(legendFillColor);
+  leg2->SetFillColor(0);
+  leg2->SetLineColor(0);
+  leg2->SetBorderSize(0);
+  leg2->SetTextFont(62);
+  leg2->SetTextSize(0.03);
+  //    leg2->AddEntry("NULL","NLO+NLL Limits","h");
+  leg2->AddEntry("NULL", "m(#tilde{q}) >> m(#tilde{g})", "h");
+  leg2->AddEntry(curvS[0],"#sigma^{NLO-QCD}","L");
+  leg2->Draw("same");
+  
+  lat->Draw("same");
+  lat2->Draw("same");
+  
+  can_exclusionOnLimit->RedrawAxis();
+  
+  if(print) {
+    can_exclusionOnLimit->Print("",".gif");
+    can_exclusionOnLimit->Print("",".pdf");
   }
-
-
+  
+  TFile* fsms = new TFile(output_dir+ "/hist_sms_gg_1jet_output.root","RECREATE");
+  fsms->cd();
+  h_xs[1]->Write("h2_95CL_limit");
+  exp1sigma_aroundExp->Write("contour_exp_1s_band");
+  curvS[3]->Write("contour_exp");
+  curvS[4]->Write("contour_exp_1s_down");
+  curvS[5]->Write("contour_exp_1s_up");
+  curvS[1]->Write("contour_theory_obs_1s_down");
+  curvS[2]->Write("contour_theory_obs_1s_up");
+  curvS[0]->Write("contour_obs");
+  h_xs[0]->Write("observed_limit_in_xsecUnit");
+  h_xs[1]->Write("expected_limit_in_xsecUnit");
+  h_limit[0]->Write("observed_limit_in_R");
+  h_limit[1]->Write("observed_limit_1s_down_in_R");
+  h_limit[2]->Write("observed_limit_1s_up_in_R");
+  h_limit[3]->Write("expected_limit_in_R");
+  h_limit[4]->Write("expected_limit_1s_down_in_R");
+  h_limit[5]->Write("expected_limit_1s_up_in_R");
+  can_exclusionOnLimit->Write();
+  fsms->Write();
+  fsms->Close();
+  
+  
   fout->cd();
   fout->Write();
-
+  
   can_limit->Write();
   can_excl01->Write();
   can_excl02->Write();
-  if(scan.Contains("sms_gg")) can_exclusionOnLimit->Write();
-
+  can_exclusionOnLimit->Write();
+  
   for(int i=0; i<nlimit; i++){
     curv[i]->Write();
     curvS[i]->Write();
