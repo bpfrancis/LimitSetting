42c207c77c61486eba18b1f70c3ab3ae3107c871
bpf6qc <bpf6qc@virginia.edu>
2014-08-19 15:34:02 -0400

    fix

--- makeTemplate_shapes.h
+++ makeTemplate_shapes.h
@@ -263,7 +263,7 @@ void GridPoint::Print() {
   if(signalYield_muon > epsilon) {
     outfile << "\t-";
     for(int i = 0; i < nBackgrounds; i++) {
-      if(i == 0 || i == 5) outfile << "replaceme";
+      if(i == 0 || i == 5) outfile << "\treplaceme";
       else outfile << "\t-";
     }
   }
